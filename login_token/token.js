8d5dbb8e70f8026184f28b0963682e408a3c0f50
Julio Cezar Souza Silva <00001126547529sp@aluno.educacao.sp.gov.br>
2026-03-23 11:17:30 -0300

    Reajuste da Pasta

--- login_token/token.js
+++
@@ -1,5 +0,0 @@
-const btnVoltar = document.getElementById("btnVoltar");
-
-btnVoltar.addEventListener("click", function() {
-    window.location.href = "../login_escreventes/login_escreventes.html";
-});
