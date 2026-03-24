22d585b1b17e99af01c600b09c93150e5d0a1b68
BRUNO HENRIQUE BALSAN <00001109645764sp@aluno.educacao.sp.gov.br>
2026-03-24 08:34:31 -0300

    Primeira versão do formulário de casamento

---
+++ formulario_1/formulario_1.js
@@ -0,0 +1,17 @@
+document.getElementById("proximo").onclick = function(){
+    window.location.href = "../formulario_2/formulario_2.html";
+    }
+
+    function calcularIdade(dataNascimento) {
+        const hoje = new Date();
+        const nascimento = new Date(dataNascimento);
+    
+        let idade = hoje.getFullYear() - nascimento.getFullYear();
+        const mes = hoje.getMonth() - nascimento.getMonth();
+    
+        if (mes < 0 || (mes === 0 && hoje.getDate() < nascimento.getDate())) {
+            idade--;
+        }
+    
+        return idade;
+    }
---
+++ formulario_2/formulario_2.js
@@ -0,0 +1,7 @@
+document.getElementById("voltar").onclick = function() {
+    window.location.href = "../formulario_1/formulario_1.html";
+};
+
+document.getElementById("proximo").onclick = function() {
+    window.location.href = "../formulario_3.html";
+};
